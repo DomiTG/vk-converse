50463fd8c2854a83f1629a75b33c3eac3793b993
Dominik Hůla <81680504+DomiTG@users.noreply.github.com>
2024-12-03 21:04:51 +0100

    host fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,17 @@
-export default function Home() {
-  const getHost = () => {
-    return window.location.host;
-  };
+import type { GetServerSideProps } from "next";
 
+type Props = { host: string | null };
+
+export const getServerSideProps: GetServerSideProps<Props> = async (
+  context,
+) => ({ props: { host: context.req.headers.host || null } });
+
+export default function Home({ host }: Props) {
   return (
     <div>
       <h1 className="text-xl">Oh, nice. it works!</h1>
       <p className="text-sm">
-        Currently, you are on <code>{getHost()}</code>
+        Currently, you are on <code>{host}</code>
       </p>
     </div>
   );
