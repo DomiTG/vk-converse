4de336c0218fd3e8e28069be869c555a97bcb90e
Dominik Hůla <81680504+DomiTG@users.noreply.github.com>
2024-12-04 20:24:12 +0100

    Domain Not Found Page

---
+++ src/components/DomainNotFoundComponent.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+
+export default function DomainNotFoundComponent({ host }: { host: string }) {
+  return (
+    <div className="min-h-screen w-full flex items-center justify-center bg-neutral-900 bg-gradient-to-br from-neutral-800 to-neutral-900">
+      <div className="backdrop-blur-lg bg-neutral-800/60 max-w-md w-full p-8 rounded-xl border border-neutral-700">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold text-white uppercase">
+            Vytvorkonverzku
+          </h1>
+          <p className="mt-4 text-sm text-neutral-400">
+            The domain{" "}
+            <span className="font-semibold text-white text-md">{host}</span> is
+            connected but not recognized by{" "}
+            <span className="font-bold text-white uppercase text-md tracking-wide">
+              Vytvorkonverzku
+            </span>
+            .
+          </p>
+          <div className="mt-6">
+            <Link
+              href="/"
+              className="inline-block px-4 py-2 text-sm text-neutral-900 bg-white rounded-lg shadow-sm hover:bg-neutral-200 transition uppercase font-semibold"
+            >
+              Go to dashboard
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,38 +2,34 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import type { NextPageContext } from "next";
 import prisma from "@/lib/prisma";
+import DomainNotFoundComponent from "@/components/DomainNotFoundComponent";
 
-type Props = { host: string | null };
+type Props = { host: object | null; originalHost: string };
 
 export default function MyApp({
   Component,
   pageProps,
   host,
+  originalHost,
 }: AppProps & Props) {
-  console.log("server-side resolved host:", host);
-
   if (host === null) {
-    return <div>404 - Domain Not Found</div>;
+    return <DomainNotFoundComponent host={originalHost} />;
   }
-
   return <Component {...pageProps} />;
 }
 
 MyApp.getInitialProps = async ({ ctx }: { ctx: NextPageContext }) => {
   const host = ctx.req?.headers.host;
   if (!host) {
-    console.log("no host");
-    return { host: null };
+    return { host: null, originalHost: null };
   }
-  console.log(host.toLowerCase());
   try {
     const domain = await prisma.page_domains.findFirst({
       where: { domain: host.toLowerCase() },
     });
     console.log(domain);
-    return { host: domain ? domain : null };
+    return { host: domain ? domain : null, originalHost: host };
   } catch (error) {
-    console.error(error);
-    return { host: null };
+    return { host: null, originalHost: host };
   }
 };
