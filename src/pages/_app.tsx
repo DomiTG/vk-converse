cedb9f4509294f8e5ebf3f1d875de7d84ad2e67c
Dominik Hůla <81680504+DomiTG@users.noreply.github.com>
2024-12-04 19:49:57 +0100

    test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,47 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import type { GetServerSideProps } from "next";
+import prisma from "@/lib/prisma";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+type Props = { host: string | null };
+
+export const getServerSideProps: GetServerSideProps<Props> = async (
+  context,
+) => {
+  const host = context.req.headers.host;
+  try {
+    const res = await prisma.page_domains.findFirst({
+      where: {
+        domain: host,
+      },
+    });
+    if (res) {
+      return {
+        props: {
+          host: res.domain,
+        },
+      };
+    } else {
+      return {
+        props: {
+          host: null,
+        },
+      };
+    }
+  } catch (error) {
+    console.error(error);
+    return {
+      props: {
+        host: null,
+      },
+    };
+  }
+};
+
+export default function MyApp({
+  Component,
+  pageProps,
+  host,
+}: AppProps & Props) {
+  return host === null ? <div>404</div> : <Component {...pageProps} />;
 }
